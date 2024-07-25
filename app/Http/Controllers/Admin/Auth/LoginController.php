85746c363a7e62f3e26f0ccc85697c903002717e
Janna Joyce E. Jumao-as <jannajoyce.jumao-as@g.msuiit.edu.ph>
2024-07-25 12:29:08 +0800

    AdminInventories

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('admin.dashboard', absolute: false));
+        return redirect()->intended(route('admin.dashboard'));
     }
 
     /**
